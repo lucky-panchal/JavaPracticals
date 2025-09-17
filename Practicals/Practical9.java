74a6ffe3f2a2deb28518ec08b9aedfc0765e7133
lucky-panchal <luckykanti31122006@gmail.com>
2025-09-17 14:39:42 +0530

    Practical 9 has done

--- Practicals/Practical9.java
+++ Practicals/Practical9.java
@@ -1,1 +1,26 @@
 //Write a Java Program to Count Number of Words in Given String.
+import java.util.Scanner;
+
+public class Practical9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        
+        int wordCount = countWords(str);
+        
+        System.out.println("Number of words: " + wordCount);
+        
+        sc.close();
+    }
+    
+    public static int countWords(String str) {
+        if (str == null || str.trim().isEmpty()) {
+            return 0;
+        }
+        
+        String[] words = str.trim().split("\\s+");
+        return words.length;
+    }
+}
